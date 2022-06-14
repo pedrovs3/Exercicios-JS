89f0fa50b1dd14482f7233da74b31b38859a5334
Pedro Vieira <pedrovs3@hotmail.com>
2022-06-14 08:23:50 -0300

    Temporary files

---
+++ aula 45/script.js
@@ -0,0 +1,12 @@
+// const nomes = ['Eduardo', 'Maria', 'Joana', 'Wallace', 'Rosana'];
+// const removidoUltimo = nomes.pop();
+
+// const novo = nomes.slice(0, -1)
+
+// console.log(nomes);
+// console.log(novo);
+
+const nomes = [ 'Pedro', 'Henrique' ];
+const nome = nomes.join(' ');
+
+console.log(nomes);
---
+++ aula46/script.js
@@ -0,0 +1,5 @@
+const nomes = ['Maria', 'Joao', 'Eduardo', 'Gabriel', 'Julia'];
+
+//const removidos = nomes.splice(3, 2);
+nomes.splice(2, 0, 'Pedro');
+console.log(nomes);
