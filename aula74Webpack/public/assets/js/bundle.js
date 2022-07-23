13de05410884fc6556edf5adccf1b1e861bc71b0
Pedro Vieira <pedrovs3@hotmail.com>
2022-07-23 19:24:58 -0300

    Webpack

---
+++ aula74Webpack/public/assets/js/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+alert('Olá Mundo');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
